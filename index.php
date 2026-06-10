e5b32a1f4b82df5b74e31297c391595e19e92c3c
Jens Scherbl <jens@scherbl.com>
2026-06-10 10:27:00 +0200

    Uses virtual page for automatic caching

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Kensho\Sitemap\Template\Sitemap;
 use Kirby\Cms\App;
+use Kirby\Cms\Page;
 use Kirby\Cms\Pages;
 use Kirby\Cms\Response;
 use Kirby\Cms\Site;
@@ -14,17 +15,22 @@ App::plugin(name: 'kensho/sitemap', extends: [
 	'collections' => [
 		'sitemap' => fn(Site $site): Pages => $site->index()->listed(),
 	],
+	'controllers' => [
+		'sitemap' => fn(App $kirby, Page $page): array => [
+			'view' => new Sitemap(app: $kirby),
+		],
+	],
 	'routes' => fn(App $kirby): array => [
 		[
 			'pattern' => 'sitemap.xml',
-			'action' => fn(): Response => new Response(body: $kirby
-				->template(name: 'sitemap', type: 'xml', defaultType: 'xml')
-				->render(data: [
-					'view' => new Sitemap(app: $kirby),
-				]), type: 'application/xml'),
+			'action' => fn(): Response => new Response(
+				body: new Page(props: ['slug' => 'sitemap', 'template' => 'sitemap'])->render(contentType: 'xml'),
+				type: 'application/xml',
+			),
 		],
 	],
 	'templates' => [
-		'sitemap' => "$root/templates/sitemap.xml.php",
+		'sitemap' => "$root/templates/sitemap.php",
+		'sitemap.xml' => "$root/templates/sitemap.xml.php",
 	],
 ]);
